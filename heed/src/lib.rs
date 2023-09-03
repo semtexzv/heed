743e825a97fcfec172941e8c3cc8d9e0cf889a68
Michal Hornický <semtexzv@gmail.com>
2023-09-03 17:20:07 +0200

    Test

--- heed/src/lib.rs
+++ heed/src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(associated_type_bounds)]
 //! Crate `heed` is a high-level wrapper of [LMDB], high-level doesn't mean heavy (think about Rust).
 //!
 //! It provides you a way to store types in LMDB without any limit and with a minimal overhead as possible,
